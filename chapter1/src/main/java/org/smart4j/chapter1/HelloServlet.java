9de93d7032581904d5f27fddcb185b862e3840b7
caowei <307118367@qq.com>
2017-11-17 17:44:19 +0800

    完成chapter1

---
+++ chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,27 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by caowei on 2017/11/17.
+ */
+@WebServlet(name = "HelloServlet",value = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        request.setAttribute("currentTime",currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
---
+++ chapter1/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: caowei
+  Date: 2017/11/17
+  Time: 17:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+    <h1>当前时间:${currentTime}</h1>
+</head>
+<body>
+
+</body>
+</html>
